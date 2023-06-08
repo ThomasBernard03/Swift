b6af60e1d274ebb5afc7bdb55dcef94011128ea9
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-06-08 15:13:30 +0200

    First application

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Thomas Bernard on 07/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
