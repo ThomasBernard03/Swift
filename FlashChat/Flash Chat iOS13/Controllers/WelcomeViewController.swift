fe6fdc69ddf9b908047fad3adaf24df720dbad62
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-06-27 13:37:08 +0200

    hide navigation bar

--- FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -31,5 +31,16 @@ class WelcomeViewController: UIViewController {
        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
 
 }
